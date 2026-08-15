ce59e9ad3f0c82531f81b9c056bddf37f871f9d9
Laetitia <laeptitchat29@gmail.com>
2026-08-15 15:23:27 +0200

    create database + add adminer

---
+++ backend/app/models/product.py
@@ -0,0 +1,39 @@
+from sqlalchemy import String, Text, Float, Integer, Boolean
+from sqlalchemy.orm import Mapped, mapped_column
+
+from database import Base
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    name: Mapped[str] = mapped_column(
+        String(150),
+        nullable=False
+    )
+
+    description: Mapped[str | None] = mapped_column(
+        Text,
+        nullable=True
+    )
+
+    price: Mapped[float] = mapped_column(
+        Float,
+        nullable=False
+    )
+
+    stock: Mapped[int] = mapped_column(
+        Integer,
+        default=0
+    )
+
+    image: Mapped[str | None] = mapped_column(
+        String(500),
+        nullable=True
+    )
+
+    customizable: Mapped[bool] = mapped_column(
+        Boolean,
+        default=False
+    )
