55d3ed2582cac856aa735e116e228553147e6c0e
Laetitia <laeptitchat29@gmail.com>
2026-08-20 20:55:09 +0200

    delete update

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,14 +6,8 @@ from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
 
-if DATABASE_URL and DATABASE_URL.startswith("postgresql://"):
-    DATABASE_URL = DATABASE_URL.replace(
-        "postgresql://",
-        "postgresql+psycopg://",
-        1
-    )
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
