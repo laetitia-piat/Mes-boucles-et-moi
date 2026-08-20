58f06385d14a874bbd1d3ba82289acce562bdfe3
Laetitia <laeptitchat29@gmail.com>
2026-08-20 21:14:45 +0200

    test for railway

--- backend/app/database.py
+++ backend/app/database.py
@@ -9,6 +9,16 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is missing")
+
+if DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgresql://",
+        "postgresql+psycopg://",
+        1,
+    )
+
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
