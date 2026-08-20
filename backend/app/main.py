fe1e820885cb5dc581a95c8015fee532bf77eb2d
Laetitia <laeptitchat29@gmail.com>
2026-08-20 19:07:02 +0200

    for update back to railway

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,6 +15,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "https://mes-boucles-et-moi.vercel.app",
+        
     ],
     allow_credentials=True,
     allow_methods=["*"],
