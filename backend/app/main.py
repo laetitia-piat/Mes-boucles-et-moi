eb47ed595bd6b32beb7dd964752cdf12bcc3e96d
Laetitia <laeptitchat29@gmail.com>
2026-08-20 17:28:21 +0200

    update cors origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,6 +14,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "https://mes-boucles-et-moi.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
