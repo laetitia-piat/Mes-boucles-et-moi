9fe0455165d8d4060cbf76032b2cb929d292e59d
Laetitia <laeptitchat29@gmail.com>
2026-08-20 22:12:23 +0200

    update style herosection on phone screen

--- frontend/components/heroSection.tsx
+++ frontend/components/heroSection.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export function HeroSection() {
   return (
     <section className="relative mb-10 overflow-hidden border-b border-[#efd8d2] bg-[radial-gradient(circle_at_center,#fffdfa_0%,#fff8f4_55%,#fceae7_100%)]">
-      <div className="  mx-auto grid min-h-[430px] max-w-[1450px] items-center px-4 py-10 sm:px-6 lg:min-h-[350px] lg:grid-cols-2 lg:px-10">
+      <div className="  mx-auto grid min-h-[150px] max-w-[1450px] items-center px-4 py-10 sm:px-6 lg:min-h-[350px] lg:grid-cols-2 lg:px-10">
         <Image
           src="/images/hero-background.png"
           alt="Mes Boucles et Moi"
@@ -14,7 +14,8 @@ export function HeroSection() {
         />
         <Link
           href="/boutique"
-          className="absolute bottom-1 left-1/3 -translate-x-1/2 rounded-lg bg-gradient-to-r from-[#df6880] to-[#f09a9d] px-6 py-3 text-white shadow-lg"
+          className="absolute bottom-1 left-1/2 -translate-x-1/2 whitespace-nowrap rounded-lg bg-gradient-to-r from-[#df6880] to-[#f09a9d] px-4 py-2
+          text-sm shadow-lg md:bottom-1 md:left-1/3 md:px-6 md:py-3 md:text-base"
         >
           DÉCOUVRIR LA BOUTIQUE
         </Link>
